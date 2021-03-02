ff01f2403a0f81fd53eca133ff195dab48ccbc12
Petr Velička <petrvelicka@tuta.io>
2021-03-02 09:21:24 +0100

    less code reduplication

--- src/kernel/kbd.c
+++ src/kernel/kbd.c
@@ -10,127 +10,87 @@
 #include <string.h>
 
 static uint32_t kb_mode = 0;
+static char kb_map[128] =
+    {
+        0,    0x1b, /* esc */
+        '1',  '2',  '3',  '4',  '5', '6', '7',  '8', '9', '0',
+        '-',  '=',  '\b', '\t', 'q', 'w', 'e',  'r', 't',
 #ifdef QWERTZ
-static char kb_map[128] = {
-    0,    0x1b, /* esc */
-    '1',  '2',  '3',  '4',  '5', '6', '7',  '8', '9', '0',
-    '-',  '=',  '\b', '\t', 'q', 'w', 'e',  'r', 't', 'z',
-    'u',  'i',  'o',  'p',  '[', ']', '\n', 0, /* left ctrl */
-    'a',  's',  'd',  'f',  'g', 'h', 'j',  'k', 'l', ';',
-    '\'', '`',  0, /* left shift */
-    '\\', 'y',  'x',  'c',  'v', 'b', 'n',  'm', ',', '.',
-    '/',  0,                                             /* right shift */
-    '*',  0,                                             /* alt */
-    ' ',                                                 /* space*/
-    0,                                                   /* capslock */
-    0,    0,    0,    0,    0,   0,   0,    0,   0,   0, /* f1 ... f10 */
-    0,                                                   /* num lock*/
-    0,                                                   /* scroll Lock */
-    0,                                                   /* home key */
-    0,                                                   /* up arrow */
-    0,                                                   /* page up */
-    '-',  0,                                             /* left arrow */
-    0,    0,                                             /* right arrow */
-    '+',  0,                                             /* end key*/
-    0,                                                   /* down arrow */
-    0,                                                   /* page down */
-    0,                                                   /* insert key */
-    0,                                                   /* delete key */
-    0,    0,    0,    0,                                 /* f11 key */
-    0,                                                   /* f12 key */
-    0, /* all other keys are undefined */
-};
-
-static char kb_shift_map[128] = {
-    0,    0x1b, /* esc */
-    '!',  '@',  '#',  '$',  '%', '^', '&',  '*', '(', ')',
-    '_',  '+',  '\b', '\t', 'Q', 'W', 'E',  'R', 'T', 'Z',
-    'U',  'I',  'O',  'P',  '{', '}', '\n', 0, /* left control */
-    'A',  'S',  'D',  'F',  'G', 'H', 'J',  'K', 'L', ':',
-    '\"', '~',  0, /* left shift */
-    '|',  'Y',  'X',  'C',  'V', 'B', 'N',  'M', '<', '>',
-    '?',  0,                                             /* right shift */
-    '*',  0,                                             /* alt */
-    ' ',                                                 /* space bar */
-    0,                                                   /* caps lock */
-    0,    0,    0,    0,    0,   0,   0,    0,   0,   0, /* f1 ... f10 */
-    0,                                                   /* num lock*/
-    0,                                                   /* scroll lock */
-    0,                                                   /* home key */
-    0,                                                   /* up arrow */
-    0,                                                   /* page up */
-    '-',  0,                                             /* left arrow */
-    0,    0,                                             /* right arrow */
-    '+',  0,                                             /* end key*/
-    0,                                                   /* down arrow */
-    0,                                                   /* page down */
-    0,                                                   /* insert key */
-    0,                                                   /* delete key */
-    0,    0,    0,    0,                                 /* f11 key */
-    0,                                                   /* f12 key */
-    0, /* all other keys are undefined */
-};
+        'z',
 #else
-static char kb_map[128] = {
-    0,    0x1b, /* esc */
-    '1',  '2',  '3',  '4',  '5', '6', '7',  '8', '9', '0',
-    '-',  '=',  '\b', '\t', 'q', 'w', 'e',  'r', 't', 'y',
-    'u',  'i',  'o',  'p',  '[', ']', '\n', 0, /* left ctrl */
-    'a',  's',  'd',  'f',  'g', 'h', 'j',  'k', 'l', ';',
-    '\'', '`',  0, /* left shift */
-    '\\', 'z',  'x',  'c',  'v', 'b', 'n',  'm', ',', '.',
-    '/',  0,                                             /* right shift */
-    '*',  0,                                             /* alt */
-    ' ',                                                 /* space*/
-    0,                                                   /* capslock */
-    0,    0,    0,    0,    0,   0,   0,    0,   0,   0, /* f1 ... f10 */
-    0,                                                   /* num lock*/
-    0,                                                   /* scroll Lock */
-    0,                                                   /* home key */
-    0,                                                   /* up arrow */
-    0,                                                   /* page up */
-    '-',  0,                                             /* left arrow */
-    0,    0,                                             /* right arrow */
-    '+',  0,                                             /* end key*/
-    0,                                                   /* down arrow */
-    0,                                                   /* page down */
-    0,                                                   /* insert key */
-    0,                                                   /* delete key */
-    0,    0,    0,    0,                                 /* f11 key */
-    0,                                                   /* f12 key */
-    0, /* all other keys are undefined */
+        'y',
+#endif
+        'u',  'i',  'o',  'p',  '[', ']', '\n', 0, /* left ctrl */
+        'a',  's',  'd',  'f',  'g', 'h', 'j',  'k', 'l', ';',
+        '\'', '`',  0, /* left shift */
+        '\\',
+#ifdef QWERTZ
+        'y',
+#else
+        'z',
+#endif
+        'x',  'c',  'v',  'b',  'n', 'm', ',',  '.', '/', 0, /* right shift */
+        '*',  0,                                             /* alt */
+        ' ',                                                 /* space*/
+        0,                                                   /* capslock */
+        0,    0,    0,    0,    0,   0,   0,    0,   0,   0, /* f1 ... f10 */
+        0,                                                   /* num lock*/
+        0,                                                   /* scroll Lock */
+        0,                                                   /* home key */
+        0,                                                   /* up arrow */
+        0,                                                   /* page up */
+        '-',  0,                                             /* left arrow */
+        0,    0,                                             /* right arrow */
+        '+',  0,                                             /* end key*/
+        0,                                                   /* down arrow */
+        0,                                                   /* page down */
+        0,                                                   /* insert key */
+        0,                                                   /* delete key */
+        0,    0,    0,    0,                                 /* f11 key */
+        0,                                                   /* f12 key */
+        0, /* all other keys are undefined */
 };
 
-static char kb_shift_map[128] = {
-    0,    0x1b, /* esc */
-    '!',  '@',  '#',  '$',  '%', '^', '&',  '*', '(', ')',
-    '_',  '+',  '\b', '\t', 'Q', 'W', 'E',  'R', 'T', 'Y',
-    'U',  'I',  'O',  'P',  '{', '}', '\n', 0, /* left control */
-    'A',  'S',  'D',  'F',  'G', 'H', 'J',  'K', 'L', ':',
-    '\"', '~',  0, /* left shift */
-    '|',  'Z',  'X',  'C',  'V', 'B', 'N',  'M', '<', '>',
-    '?',  0,                                             /* right shift */
-    '*',  0,                                             /* alt */
-    ' ',                                                 /* space bar */
-    0,                                                   /* caps lock */
-    0,    0,    0,    0,    0,   0,   0,    0,   0,   0, /* f1 ... f10 */
-    0,                                                   /* num lock*/
-    0,                                                   /* scroll lock */
-    0,                                                   /* home key */
-    0,                                                   /* up arrow */
-    0,                                                   /* page up */
-    '-',  0,                                             /* left arrow */
-    0,    0,                                             /* right arrow */
-    '+',  0,                                             /* end key*/
-    0,                                                   /* down arrow */
-    0,                                                   /* page down */
-    0,                                                   /* insert key */
-    0,                                                   /* delete key */
-    0,    0,    0,    0,                                 /* f11 key */
-    0,                                                   /* f12 key */
-    0, /* all other keys are undefined */
-};
+static char kb_shift_map[128] =
+    {
+        0,    0x1b, /* esc */
+        '!',  '@',  '#',  '$',  '%', '^', '&',  '*', '(', ')',
+        '_',  '+',  '\b', '\t', 'Q', 'W', 'E',  'R', 'T',
+#ifdef QWERTZ
+        'Z',
+#else
+        'Y',
 #endif
+        'U',  'I',  'O',  'P',  '{', '}', '\n', 0, /* left control */
+        'A',  'S',  'D',  'F',  'G', 'H', 'J',  'K', 'L', ':',
+        '\"', '~',  0, /* left shift */
+        '|',
+#ifdef QWERTZ
+        'Z',
+#else
+        'Y',
+#endif
+        'X',  'C',  'V',  'B',  'N', 'M', '<',  '>', '?', 0, /* right shift */
+        '*',  0,                                             /* alt */
+        ' ',                                                 /* space bar */
+        0,                                                   /* caps lock */
+        0,    0,    0,    0,    0,   0,   0,    0,   0,   0, /* f1 ... f10 */
+        0,                                                   /* num lock*/
+        0,                                                   /* scroll lock */
+        0,                                                   /* home key */
+        0,                                                   /* up arrow */
+        0,                                                   /* page up */
+        '-',  0,                                             /* left arrow */
+        0,    0,                                             /* right arrow */
+        '+',  0,                                             /* end key*/
+        0,                                                   /* down arrow */
+        0,                                                   /* page down */
+        0,                                                   /* insert key */
+        0,                                                   /* delete key */
+        0,    0,    0,    0,                                 /* f11 key */
+        0,                                                   /* f12 key */
+        0, /* all other keys are undefined */
+};
 
 char currkey;
 
