28ca5cb5471a7018ed64f17e93097f1c96ff0686
sitting33 <me@33rd.dev>
2021-03-04 08:54:39 +0300

    add more i/o port sizes and make them inline

--- src/include/hw.h
+++ src/include/hw.h
@@ -3,9 +3,15 @@
 
 #include <stdint.h>
 
-unsigned char inb(unsigned short _port);
-void outb(unsigned short _port, unsigned char _data);
+uint8_t inb(uint16_t _port);
+void outb(uint16_t _port, uint8_t _data);
+
 uint16_t inw(uint16_t _port);
+void outw(uint16_t _port, uint16_t _data);
+
+uint32_t inl(uint16_t _port);
+void outl(uint16_t _port, uint32_t _data);
+
 void io_wait(void);
 
 #endif // !__HW_H__
