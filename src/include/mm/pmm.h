44c1c944cea7b9be445d2d1d0b8d05d762bd6b1c
xlatbptr <80234531+xlatbptr@users.noreply.github.com>
2021-03-26 10:31:51 -0400

    Basic structures

--- src/include/mm/pmm.h
+++ src/include/mm/pmm.h
@@ -4,6 +4,23 @@
 #include <boot/stivale2.h>
 #include <stddef.h>
 
+enum {
+  HBLOCK_FREE = 0,
+  HBLOCK_USED = 1,
+  HBLOCK_NOT_PRESENT = 2,
+};
+
+struct heap_master {
+  size_t n_blocks;
+  struct heap_block blocks[];
+};
+
+struct heap_block {
+  size_t size;
+  unsigned char type;
+  struct heap_block * next;
+};
+
 void pmm_init(struct stivale2_mmap_entry *memory_map, size_t memory_entries);
 
 #endif // !__PMM_H__
